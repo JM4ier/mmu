36cacb1d9282cdc09b5cadb7899ba9a978d93f5e
Jonas Maier <>
2023-12-06 19:30:12 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,197 @@
+use std::{ops::{Index, IndexMut}, fmt::Display};
+
+#[derive(Copy, Clone)]
+pub struct PhysAddr(u64);
+
+impl Display for PhysAddr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "P-{:160x}", self.0)
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct VirtAddr(u64);
+
+impl VirtAddr {
+    pub const fn page_offset(self) -> u64 {
+        self.0 & 4095
+    }
+    pub const fn virtual_page_number(self) -> u64 {
+        self.0 >> 12
+    }
+    pub const fn vpn1(self) -> usize {
+        ((self.0 >> 12) & 511) as usize
+    }
+    pub const fn vpn2(self) -> usize {
+        ((self.0 >> 21) & 511) as usize
+    }
+    pub const fn vpn3(self) -> usize {
+        ((self.0 >> 30) & 511) as usize
+    }
+    pub const fn vpn4(self) -> usize {
+        ((self.0 >> 39) & 511) as usize
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct PageTableEntry {
+    bits: u64,
+}
+
+impl PageTableEntry {
+    pub fn is_present(self) -> bool {
+        self.bits & 1 > 0
+    }
+    pub fn phys_addr(self) -> PhysAddr {
+        PhysAddr(self.bits & 0x000FFFFFFFFFFFF000)
+    }
+}
+
+pub type PageTable = [PageTableEntry; 512];
+
+pub struct L1dCacheEntry {
+    valid: bool,
+    tag: u64,
+    line: [u8; 64],
+}
+
+pub struct L1dCacheSet {
+    entries: [L1dCacheEntry; 8],
+}
+
+pub struct L1dCache {
+    entries: [L1dCacheSet; 64],
+}
+
+pub struct Memory {
+    memory: Vec<u8>,
+}
+
+impl Memory {
+    fn read<T: Copy>(&self, addr: PhysAddr) -> T {
+        let a = addr.0 as usize;
+        let len = core::mem::size_of::<T>();
+
+        if a + len > self.memory.len() {
+            panic!("memory access out of bounds...");
+        }
+
+        let addr = (&self.memory[a]) as *const u8 as *const T;
+
+        unsafe { *addr }
+    }
+}
+
+pub struct TlbEntry {
+    valid: bool,
+    access: u8,
+    tag: u64,
+    addr: PhysAddr,
+}
+impl TlbEntry {
+    pub fn mark_accessed(&mut self) {
+        self.access = self.access.saturating_add(1);
+    }
+}
+
+pub type TlbEntrySet = [TlbEntry; 4];
+
+pub struct Tlb {
+    sets: [TlbEntrySet; 128],
+}
+impl Index<u64> for Tlb {
+    type Output = TlbEntrySet;
+
+    fn index(&self, index: u64) -> &Self::Output {
+        &self.sets[index as usize]
+    }
+}
+impl IndexMut<u64> for Tlb {
+    fn index_mut(&mut self, index: u64) -> &mut Self::Output {
+        &mut self.sets[index as usize]
+    }
+}
+
+pub struct Machine {
+    cr3: PhysAddr,
+    tlb: Tlb,
+    memory: Memory,
+    cache: L1dCache,
+}
+
+pub struct PageFault;
+
+impl Machine {
+    pub fn translate(&mut self, virt_addr: VirtAddr) -> Result<PhysAddr, PageFault> {
+        let tlb_index = virt_addr.virtual_page_number() & 127;
+        let tlb_tag = virt_addr.virtual_page_number() >> 7;
+        let tlb_set = &mut self.tlb[tlb_index];
+
+        for i in 0..4 {
+            if tlb_set[i].tag == tlb_tag && tlb_set[i].valid {
+                tlb_set[i].mark_accessed();
+                return Ok(tlb_set[i].addr);
+            }
+        }
+
+        let addr1 = self.cr3;
+        let page_table_1 = self.memory.read::<PageTable>(addr1);
+        let pte1 = page_table_1[virt_addr.vpn1()];
+        if !pte1.is_present() {
+            return Err(PageFault);
+        }
+
+        let addr2 = pte1.phys_addr();
+        let page_table_2 = self.memory.read::<PageTable>(addr2);
+        let pte2 = page_table_2[virt_addr.vpn2()];
+        if !pte2.is_present() {
+            return Err(PageFault);
+        }
+
+        let addr3 = pte2.phys_addr();
+        let page_table_3 = self.memory.read::<PageTable>(addr3);
+        let pte3 = page_table_3[virt_addr.vpn3()];
+        if !pte3.is_present() {
+            return Err(PageFault);
+        }
+
+        let addr4 = pte3.phys_addr();
+        let page_table_4 = self.memory.read::<PageTable>(addr4);
+        let pte4 = page_table_4[virt_addr.vpn4()];
+        if !pte4.is_present() {
+            return Err(PageFault);
+        }
+
+        let phys_addr = pte4.phys_addr();
+
+        let mut k = 0;
+        for i in 0..4 {
+            if !tlb_set[i].valid {
+                // if not valid, chose this entry
+                k = i;
+                break;
+            }
+
+            // choose entry with least accesses
+            if tlb_set[i].access < tlb_set[k].access {
+                k = i;
+            }
+        }
+
+        for i in 0..4 {
+            // reset access counter
+            tlb_set[i].access = 0;
+        }
+
+        // replace old entry
+        tlb_set[k].valid = true;
+        tlb_set[k].addr = phys_addr;
+        tlb_set[k].tag = tlb_tag;
+
+        Ok(phys_addr)
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
